0199b8cf5b35b58bc1de75d778b0f1e59c0a047f
Shruti Kapoor <shrkapoor@paypal.com>
2019-06-13 23:34:41 -0700

    wip

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -6,25 +6,28 @@ import Button from "muicss/lib/react/button";
 
 const AddSong = ({onSubmit}) => {
     //add mutation to this component. Refactor it out.
+    const {name, setName} = useState("");
+    const {actor, setActor} = useState("");
+    const {lyrics, setLyrics} = useState("");
+
     return (
         <Mutation mutation={ADD_SONG}>
             {(addSong, data) => (
         <form onSubmit={e => {
-            console.log(e);
             e.preventDefault();
             addSong({ variables: { song: {
-                        name: e.name,
-                        actor: e.actor,
-                        lyrics: e.lyrics
+                        name,
+                        actor,
+                        lyrics
                         }}
                     });
                 }}
             >
             <div className="preference">
                 <label htmlFor="addSong">Add a new song</label>
-                <input type="text" name="name" id="addSong" placeholder="name"/>
-                <input type="text" name="actor" id="actor" placeholder="actor"/>
-                <input type="text" name="lyrics" id="lyrics" placeholder="lyrics"/>
+                <input value={name} type="text" name="name" placeholder="name" onChange={e => setName(e.target.value)} />
+                <input value={actor} type="text" name="actor" placeholder="actor" onChange={e => setActor(e.target.value)}/>
+                <input value={lyrics} type="text" name="lyrics" placeholder="lyrics" onChange={e => setLyrics(e.target.value)}/>
             </div>
             <Button variant="contained" color="primary" type="submit">
                 Add Song
